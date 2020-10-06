1a74c4b17bd972fb709eeefbb7d62006a09cf51c
Ehfaz <ehfaz.rezwan@gmail.com>
2020-10-07 01:41:02 +0600

    Connected to Mongo instance and created schema

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ import express from "express";
 import mongoose from "mongoose";
 import Pusher from "pusher";
 import cors from "cors";
+
+import mongoMessages from "./messageModel.js";
+
 // app config
 const app = express();
 const port = process.env.PORT || 9000;
@@ -10,7 +13,21 @@ const port = process.env.PORT || 9000;
 
 // db config
 
+const mongoURI = `mongodb+srv://admin:0KgxP0of87xnrqPu@cluster0.kqbya.mongodb.net/messengerdb?retryWrites=true&w=majority`;
+mongoose.connect(mongoURI, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.once("open", () => {
+  console.log("DB Connected");
+});
+
 // api routes
+
 app.get("/", (req, res) => res.status(200).send("Hello world"));
+
 // listen
+
 app.listen(port, () => console.log(`listening on localhost:${port}`));
