7753c7aa42774409f4f6cefc93b720af1d3fba9e
Ehfaz <ehfaz.rezwan@gmail.com>
2020-10-07 01:02:12 +0600

    Added Hello World route

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 // import dependencies
-
+import express from "express";
+import mongoose from "mongoose";
+import Pusher from "pusher";
+import cors from "cors";
 // app config
-
+const app = express();
+const port = process.env.PORT || 9000;
 // middlewares
 
 // db config
 
 // api routes
-
+app.get("/", (req, res) => res.status(200).send("Hello world"));
 // listen
+app.listen(port, () => console.log(`listening on localhost:${port}`));
